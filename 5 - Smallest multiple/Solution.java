10799d12a43f9081cf927dea491f269b9f9905b4
Nikos Ioannou <ioannounikos93@gmail.com>
2017-07-15 01:18:15 +0300

    Create Solution.java

---
+++ 5 - Smallest multiple/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		long number = 0L;
+		for(long i = 20;i<Long.MAX_VALUE;i++){
+			boolean divisible = true;
+			for(int j = 1; j < 21 ; j++){
+				if(i % j != 0){
+					divisible = false;
+					break;
+				}
+			}
+			if(divisible){
+				number = i;
+				break;
+			}
+		}
+		
+		System.out.println(number);
+	}
+}
